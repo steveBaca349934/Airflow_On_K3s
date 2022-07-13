864c54d90f8311f4474ad917d098df1e9b8858a6
Steve Baca <sbaca@alumni.upenn.edu>
2022-07-13 10:33:29 +0000

    push

--- dags/orchestrate_mutual_fund_data_pull_dag.py
+++ dags/orchestrate_mutual_fund_data_pull_dag.py
@@ -5,15 +5,15 @@ sys.path.append("/home/oxbigsnowman/Airflow_On_Micro-k8s_Cluster/jobs/transform/
 # docker path
 sys.path.append("/opt/airflow/jobs/extract/")
 sys.path.append("/opt/airflow/jobs/transform/")
-from extract_stock_data import scrape_stock_tickers
-from transform_stock_data import get_ticker_data
+from extract_mutual_fund_data import scrape_schwab_mf_tickers, scrape_schwab_etf_tickers
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.models.xcom import XCom
 from airflow.decorators import dag, task
 import pendulum
 from datetime import timedelta
-from typing import Dict, Tuple
+from typing import Dict, Tuple, List
 
 
 @dag(
@@ -25,5 +25,24 @@ from typing import Dict, Tuple
     tags=['orchestrate_mutual_fund_data_pull_dag']
 )
 def orchestrate_mutual_fund_data_pull_dag():
-    
-    pass
+
+    @task
+    def extract_mf_tickers() -> List[str]:
+        
+        mf_ticks = scrape_schwab_mf_tickers()
+
+        return mf_ticks
+
+    @task
+    def extract_etf_tickers() -> List[str]:
+
+        etf_ticks = scrape_schwab_etf_tickers()
+
+        return etf_ticks
+
+
+
+    mf_ticks = extract_mf_tickers()
+    etf_ticks = extract_etf_tickers()
+
+mf_dag = orchestrate_mutual_fund_data_pull_dag()
